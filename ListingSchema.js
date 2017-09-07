d47476c1a5fd2b2ad1915d9230c5861509958060
tjakkala <teja.jakkala@gmail.com>
2017-09-06 23:01:26 -0400

    Update ListingSchema.js

--- ListingSchema.js
+++ ListingSchema.js
@@ -6,17 +6,21 @@ var listingSchema = new Schema({
 
   code: { type: String, required: true, unique: true },
   name: { type: String, required: true, unique: true },
+	
   coordinates: {
 	 latitude: Number,
 	 longitude: Number
   },
+	
   address: String
 
 });
 
 listingSchema.pre('save', function(next) {
   var currentDate = new Date();
+	
   this.updated_at = currentDate;
+	
   if (!this.created_at)
 	 this.created_at = currentDate;
   
